ce0c34339a947ed382aeed9e568efd6f824bd432
chris-medlin <cjmedlin99@yahoo.com>
2016-12-14 15:09:50 +0000

    Remove example command and subsystem in Robot.java and replaced with our own versions of those.
    Instead of "ExampleCommand" and "ExampleSubSystem", our Robot.java now has a "DriveTrain" and a "MoveForward". The DriveTrain class has been implemented, however I don't exactly know how our drive train is constructed yet so it will probably be changed in the future. The MoveForward class has yet to be implemented. Also, changed the .gitignore.

--- src/org/usfirst/frc/team6443/robot/Robot.java
+++ src/org/usfirst/frc/team6443/robot/Robot.java
@@ -1,8 +1,7 @@
-
 package org.usfirst.frc.team6443.robot;
 
-import org.usfirst.frc.team6443.robot.commands.ExampleCommand;
-import org.usfirst.frc.team6443.robot.subsystems.DriveTrain;
+import org.usfirst.frc.team6443.robot.commands.*;
+import org.usfirst.frc.team6443.robot.subsystems.*;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
@@ -12,98 +11,83 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
+ * The Robot class is FRC team 6443's implementation of WPIlib's IterativeRobot class. 
+ *
+ * @author Christopher Medlin
  */
 public class Robot extends IterativeRobot {
-
 	public static final DriveTrain driveTrain = new DriveTrain();
 	public static OI oi;
 
-    Command autonomousCommand;
-    SendableChooser chooser;
-    
-    /**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
-     */
-    public void robotInit() {
+	private Command autonomy;
+	private SendableChooser chooser;
+	
+	/*
+	 * Called when the robot first starts.
+	 */
+	@Override
+	public void robotInit () {
 		oi = new OI();
-        chooser = new SendableChooser();
-        chooser.addDefault("Default Auto", new ExampleCommand());
-//        chooser.addObject("My Auto", new MyAutoCommand());
-        SmartDashboard.putData("Auto mode", chooser);
-    }
+		chooser = new SendableChooser();
+		chooser.addDefault("Move Forward", new MoveForward());
+		SmartDashboard.putData("Auto", chooser);
+	}
 	
-	/**
-     * This function is called once each time the robot enters Disabled mode.
-     * You can use it to reset any subsystem information you want to clear when
-	 * the robot is disabled.
-     */
-    public void disabledInit(){
+	/*
+	 * Called when the robot first enters disabled mode.
+	 */
+	@Override
+	public void disabledInit () {
+		
+	}
+	
+	/*
+	 * Called periodically when the robot is in disabled mode.
+	 */
+	@Override
+	public void disabledPeriodic () {
+		Scheduler.getInstance().run();
+	}
+	
+	/*
+	 * Called when the robot first enters autonomous mode.
+	 */
+	@Override
+	public void autonomousInit () {
+		autonomousCommand =  (Command) chooser.getSelected();
 
-    }
-	
-	public void disabledPeriodic() {
+		if (autonomousCommand != null) autonomousCommand.start();
+	}
+
+	/*
+	 * Called periodcally when the robot is in autonomous mode.
+	 */
+	@Override
+	public void autonomousPeriodic () {
 		Scheduler.getInstance().run();
 	}
 
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
-	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
-	 * Dashboard, remove all of the chooser code and uncomment the getString code to get the auto name from the text box
-	 * below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional commands to the chooser code above (like the commented example)
-	 * or additional comparisons to the switch structure below with additional strings & commands.
+	/*
+	 * Called when the robot first enter teleop mode.
 	 */
-    public void autonomousInit() {
-        autonomousCommand = (Command) chooser.getSelected();
-        
-		/* String autoSelected = SmartDashboard.getString("Auto Selector", "Default");
-		switch(autoSelected) {
-		case "My Auto":
-			autonomousCommand = new MyAutoCommand();
-			break;
-		case "Default Auto":
-		default:
-			autonomousCommand = new ExampleCommand();
-			break;
-		} */
-    	
-    	// schedule the autonomous command (example)
-        if (autonomousCommand != null) autonomousCommand.start();
-    }
+	@Override
+	public void teleopInit () {
+		if (autonomousCommand != null) autonomousCommand.cancel();
+	}
 
-    /**
-     * This function is called periodically during autonomous
-     */
-    public void autonomousPeriodic() {
-        Scheduler.getInstance().run();
-    }
-
-    public void teleopInit() {
-		// This makes sure that the autonomous stops running when
-        // teleop starts running. If you want the autonomous to 
-        // continue until interrupted by another command, remove
-        // this line or comment it out.
-        if (autonomousCommand != null) autonomousCommand.cancel();
-    }
-
-    /**
-     * This function is called periodically during operator control
-     */
-    public void teleopPeriodic() {
-        Scheduler.getInstance().run();
-    }
-    
-    /**
-     * This function is called periodically during test mode
-     */
-    public void testPeriodic() {
-        LiveWindow.run();
-    }
+	/*
+	 * Called periodically when the robot is in teleop mode.
+	 */
+	@Override
+	public void teleopPeriodic () {
+		Scheduler.getInstance().run();
+	}
+	
+	/*
+	 * Called periodically when the robot is in testing mode.
+	 */
+	@Override
+	public void testPeriodic () {
+		LiveWindow.run();
+	}
 }
