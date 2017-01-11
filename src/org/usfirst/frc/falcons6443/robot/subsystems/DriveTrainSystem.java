9ce88fb14b5d95e17039739db1171b554a42277e
Patrick Higgins <higgp153@hsd.k12.or.us>
2017-01-11 15:53:36 -0800

    Implement VictorSPGroup in movement code

--- src/org/usfirst/frc/falcons6443/robot/subsystems/DriveTrainSystem.java
+++ src/org/usfirst/frc/falcons6443/robot/subsystems/DriveTrainSystem.java
@@ -1,17 +1,15 @@
 package org.usfirst.frc.falcons6443.robot.subsystems;
 
-import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 import org.usfirst.frc.falcons6443.robot.RobotMap;
+import org.usfirst.frc.falcons6443.robot.VictorSPGroup;
 import org.usfirst.frc.falcons6443.robot.commands.*;
-
-
 
 public class DriveTrainSystem extends Subsystem {
 
-	VictorSP[] leftMotors = RobotMap.DrivetrainVictorsLeft;
-	VictorSP[] rightMotors = RobotMap.DrivetrainVictorsRight;
+	VictorSPGroup leftMotors = new VictorSPGroup(RobotMap.DrivetrainVictorsLeft);
+	VictorSPGroup rightMotors = new VictorSPGroup(RobotMap.DrivetrainVictorsRight);
 	
 
 	public DriveTrainSystem () {
@@ -29,7 +27,7 @@ public class DriveTrainSystem extends Subsystem {
 	 * @param right right axis value.
 	 */
 	public void updateGamepadInput(double left, double right) {
-		drive(left, right);
+		tankDrive(left, right);
 	}
 
 	/**
@@ -38,10 +36,8 @@ public class DriveTrainSystem extends Subsystem {
 	 * @param left the power for the left motors.
 	 * @param right the power for the right motors.
 	 */
-	public void drive (double left, double right) {
-		for (VictorSP motor : leftMotors)
-			motor.set(left);
-		for (VictorSP motor : rightMotors)
-			motor.set(right);
+	public void tankDrive (double left, double right) {
+		leftMotors.set(left);
+		rightMotors.set(right);
 	}
 }
