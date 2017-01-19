edf6f3ee9a0f05a17715979c5455cea7bbcedbb0
Patrick Higgins <quickpanic@me.com>
2017-01-18 17:44:46 -0800

    Remove static property on gamepad IDs
    As these IDs are private and will not ever be accessed in the static context, they should not be static to protect abuse.

--- src/org/usfirst/frc/falcons6443/robot/OI.java
+++ src/org/usfirst/frc/falcons6443/robot/OI.java
@@ -16,16 +16,16 @@ import edu.wpi.first.wpilibj.GenericHID;
  */
 public class OI {
 
-	private static final int GAMEPAD_LEFT_STICK_AXIS_X_ID = 0;
-	private static final int GAMEPAD_LEFT_STICK_AXIS_Y_ID = 1;
+	private final int GAMEPAD_LEFT_STICK_AXIS_X_ID = 0;
+	private final int GAMEPAD_LEFT_STICK_AXIS_Y_ID = 1;
 	
-	private static final int GAMEPAD_RIGHT_STICK_AXIS_X_ID = 4;
-	private static final int GAMEPAD_RIGHT_STICK_AXIS_Y_ID = 5;
+	private final int GAMEPAD_RIGHT_STICK_AXIS_X_ID = 4;
+	private final int GAMEPAD_RIGHT_STICK_AXIS_Y_ID = 5;
 	
-	private static final int GAMEPAD_LEFT_TRIGGER_AXIS_ID = 2;
-	private static final int GAMEPAD_RIGHT_TRIGGER_AXIS_ID = 3;
+	private final int GAMEPAD_LEFT_TRIGGER_AXIS_ID = 2;
+	private final int GAMEPAD_RIGHT_TRIGGER_AXIS_ID = 3;
 	
-	private static final int GAMEPAD_PORT_NUMBER = 0;
+	private final int GAMEPAD_PORT_NUMBER = 0;
 
 	private Joystick joystick;
      	
