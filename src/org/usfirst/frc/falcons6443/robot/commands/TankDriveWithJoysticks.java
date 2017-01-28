18531957f478c444c9d51df3bafb22e72e535c75
Patrick Higgins <higgp153@hsd.k12.or.us>
2017-01-28 14:27:00 -0800

    Trigger variable speed update
    The right trigger makes speed to 75% vs 45?

--- src/org/usfirst/frc/falcons6443/robot/commands/TankDriveWithJoysticks.java
+++ src/org/usfirst/frc/falcons6443/robot/commands/TankDriveWithJoysticks.java
@@ -3,6 +3,8 @@ package org.usfirst.frc.falcons6443.robot.commands;
 import org.usfirst.frc.falcons6443.robot.Robot;
 import org.usfirst.frc.falcons6443.robot.hardware.Gamepad;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 /**
  * This command allows the driver to control the robot with two joysticks. Both joysticks control the motors on the side
  * of the robot respective to the joystick. So the right joystick controls the motors on the right side of the robot,
@@ -16,13 +18,13 @@ import org.usfirst.frc.falcons6443.robot.hardware.Gamepad;
 public class TankDriveWithJoysticks extends SimpleCommand {
 
 	Gamepad gamepad;
+	double leftInput, rightInput, fullPower;
 
 	public TankDriveWithJoysticks() {
 		super("Move With Joystick Using Tank Drive");
 		requires(driveTrain);
 		
-
-		
+		fullPower = 0.75d;
 	}
 	@Override
 	public void initialize () {
@@ -30,7 +32,19 @@ public class TankDriveWithJoysticks extends SimpleCommand {
 	}
 	@Override
 	public void execute () {
-		driveTrain.updateGamepadInput(gamepad.leftStickY(), gamepad.rightStickY());
+		leftInput = gamepad.leftStickY() * driveTrain.MotorPowerModifier * fullPower;
+		rightInput = gamepad.rightStickY() * driveTrain.MotorPowerModifier * fullPower;
+		
+		if (gamepad.rightTrigger() > 0) {
+			fullPower = 1.0d;
+		} else {
+			fullPower = 0.75d;
+		}
+		
+		SmartDashboard.putNumber("Left Input", leftInput);
+		SmartDashboard.putNumber("Right Input", rightInput);
+		
+		driveTrain.updateGamepadInput(adjustedInput(leftInput), adjustedInput(rightInput));
 	}
 
 	/* There are no particular conditions in which we want the command to stop autonomously. */
