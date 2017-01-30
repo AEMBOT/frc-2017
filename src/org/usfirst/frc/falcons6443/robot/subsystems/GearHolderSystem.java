df099fa0e7f88ee8fc4be254ad860e8dde600f49
chris-medlin <cjmedlin99@yahoo.com>
2017-01-30 11:10:41 -0800

    Added isOpen method to GearHolderSystem.

--- src/org/usfirst/frc/falcons6443/robot/subsystems/GearHolderSystem.java
+++ src/org/usfirst/frc/falcons6443/robot/subsystems/GearHolderSystem.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class GearHolderSystem extends Subsystem {
 	
 	private Solenoid solenoid;
+	private boolean open;
 
 	public GearHolderSystem() {
 		solenoid = new Solenoid(RobotMap.GearHolderSolenoid);
@@ -16,12 +17,18 @@ public class GearHolderSystem extends Subsystem {
 	public void initDefaultCommand () {
 		setDefaultCommand(new ReleaseGearCommand);
 	}
+	
+	public boolean isOpen () {
+		return open;
+	}
 
 	public void open () {
 		solenoid.set(true);
+		open = true;
 	}
 
 	public void close () {
 		solenoid.set(false);
+		open = false;
 	}
 }
