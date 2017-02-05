887032c425140372b763605e8180e2cffd8eeacf
Christopher Medlin <cjmedlin99@yahoo.com>
2017-02-04 16:30:06 -0800

    Improved Dustin's comments in GearHolderSystem.

--- src/org/usfirst/frc/falcons6443/robot/subsystems/GearHolderSystem.java
+++ src/org/usfirst/frc/falcons6443/robot/subsystems/GearHolderSystem.java
@@ -5,36 +5,40 @@ import org.usfirst.frc.falcons6443.robot.RobotMap;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
+/**
+ * Subsystem which contains the components of the pneumatics control system.
+ * <p>
+ *     This includes the solenoid, which is used to open or close the gear holder.
+ * </p>
+ *
+ * @author Christopher Medlin, Patrick Higgins, Shivashriganesh Mahato
+ */
 public class GearHolderSystem extends Subsystem {
 	
 	private Solenoid solenoid;
 
 	/**
-	 * maps the (variable?) solenoid to the RobotMap
+	 * Constructor for GearHolderSystem.
 	 */
 	public GearHolderSystem() {
 		solenoid = new Solenoid(RobotMap.GearHolderSolenoid);
 	}
 
-	/* (non-Javadoc)
-	 * @see edu.wpi.first.wpilibj.command.Subsystem#initDefaultCommand()
-	 */
+	@Override
 	public void initDefaultCommand () {
 		// setDefaultCommand(new ReleaseGearCommand);
 	}
 
 	/**
-	 * when the solenoid is open it's set as true
+	 * Sets the state of the GearHolderSystem to open.
 	 */
-	
 	public void open () {
 		solenoid.set(true);
 	}
 
 	/**
-	 * when the solenoid is open it's set as false
+	 * Sets the state of the GearHolderSystem to close.
 	 */
-	
 	public void close () {
 		solenoid.set(false);
 	}
