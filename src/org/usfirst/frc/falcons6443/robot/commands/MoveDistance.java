bbb79fa50ac457dbb3af9fc269e7774bdd421d5c
Christopher Medlin <christopherjmedlin@gmail.com>
2017-02-16 11:28:41 -0800

    Added MoveDistance command.

---
+++ src/org/usfirst/frc/falcons6443/robot/commands/MoveDistance.java
@@ -0,0 +1,37 @@
+package org.usfirst.frc.falcons6443.robot.commands;
+
+/**
+ * Moves the robot a distance specified in the constructor.
+ *
+ * @author Christopher Medlin
+ */
+public class MoveDistance extends SimpleCommand {
+
+    double distance;
+    double initUltrasonicReading;
+
+    /**
+     * @param distance the distance the robot is to move.
+     */
+    public MoveDistance (double distance) {
+        super("Move Distance");
+        this.distance = distance;
+        requires(driveTrain);
+        requires(navigation);
+    }
+
+    @Override
+    public void initialize () {
+        initUltrasonicReading = navigation.read("Back");
+    }
+
+    @Override
+    public void execute () {
+        navigation.ping("Back");
+    }
+
+    @Override
+    public boolean isFinished () {
+        return (navigation.read("Back") <= initUltrasonicReading + distance);
+    }
+}
