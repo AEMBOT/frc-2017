aeb7c7b8e9d7a464d70df44c0dd7a6f5c61cfd50
chris-medlin <cjmedlin99@yahoo.com>
2017-02-18 14:01:45 -0800

    Add MovePastAngledWall command.

---
+++ src/org/usfirst/frc/falcons6443/robot/commands/MovePastAngledWall.java
@@ -0,0 +1,46 @@
+package org.usfirst.frc.falcons6443.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+
+/**
+ * Created by chris-medlin on 2/18/17.
+ */
+public class MovePastAngledWall extends SimpleCommand {
+
+    private double distanceToWall;
+    private double temp;
+    private double delta;
+    private final double DELTA_TOLERANCE = 0.5;
+    private String ultra;
+
+    public MovePastAngledWall (String ultra) {
+        super("Move Past Angled Wall");
+        this.ultra = ultra;
+        requires(navigation);
+        requires(driveTrain);
+    }
+
+    @Override
+    public void initialize () {
+        driveTrain.tankDrive(1,1);
+        distanceToWall = navigation.read(ultra);
+    }
+
+    @Override
+    public void execute () {
+        temp = distanceToWall;
+        distanceToWall = navigation.read(ultra);
+        delta = distanceToWall - temp;
+    }
+
+    @Override
+    public boolean isFinished () {
+        if (delta < DELTA_TOLERANCE) {
+            driveTrain.tankDrive(0,0);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
