b40a00ba6e539413d44b6b147893e3300be13d29
Christopher Medlin <christopherjmedlin@gmail.com>
2017-02-18 11:52:08 -0800

    Add MoveUntilLineIntersection command.

---
+++ src/org/usfirst/frc/falcons6443/robot/commands/MoveUntilLineIntersection.java
@@ -0,0 +1,32 @@
+package org.usfirst.frc.falcons6443.robot.commands;
+
+import org.usfirst.frc.falcons6443.robot.utilities.Line2D;
+
+/**
+ * Moves the robot forward until a given line has been intersected.
+ *
+ * This command requires two preconditions to work properly:
+ * 1. The robot must be in a 0, 90, 180, or 270 degree angle.
+ * 2. The walls that the robot is measuring distance from mustn't be angled.
+ *
+ * It should also be noted that this command DOES NOT alter the robot's path in order
+ * to intersect a line. If a robot is moving parallel to a line, this command will
+ * never end.
+ */
+public class MoveUntilLineIntersection extends SimpleCommand {
+
+    private Line2D line;
+
+    private String yUltra;
+    private double yVal;
+
+    public MoveUntilLineIntersection(Line2D line, String yUltra, String xUltra) {
+        super("Move Until Line Intersection.");
+        this.yUltra = yUltra;
+        yVal = line.calcY(navigation.read(xUltra));
+    }
+
+    public boolean isFinished () {
+        return (navigation.read(yUltra) >= yVal);
+    }
+}
